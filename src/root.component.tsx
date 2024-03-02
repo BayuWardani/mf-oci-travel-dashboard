ec8667f3b5ded1281d942cae316f059aa0f1db94
Bayu Wardani <bayuwardani@Bayus-MacBook-Air.local>
2024-03-02 10:45:22 +0700

    d

---
+++ src/root.component.tsx
@@ -0,0 +1,22 @@
+import {Card} from '@oci-lib/ui'
+
+import '@oci-lib/ui/dist/style.css'
+
+export default function Root(props) {
+  return (
+    <>
+    <div className='d-flex align-item-center justify-content-center mt-4'>
+      <div className='col-md-5'>
+        <Card title="Peringatan">
+          <p>Pilih salah satu untuk melihat UI Microfrontend</p>
+          <div className='d-flex gap-3'>
+            <a href='/train' className='btn btn-sm btn-secondary'>MF Train</a>
+            <a href='/hotel' className='btn btn-sm btn-secondary'>MF Hotel</a>
+            <a href='/cra' className='btn btn-sm btn-secondary'>MF Existing App (sebelumnya pakai CRA)</a>
+          </div>
+        </Card>
+      </div>
+    </div>
+    </>
+  )
+}
